33ac038ccaa12df99255cc9996cc448244c27e6c
kabinh07 <kanchon@polygontech.xyz>
2025-05-31 20:30:17 +0600

    ADDED card prompts

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
 import time
-from config import LLM_MODEL, LLM_BASE_URL, LLM_TEMPERATURE
+from config import LLM_MODEL, LLM_BASE_URL, LLM_TEMPERATURE, LLM_PROMPT_PATH
+
+print(LLM_PROMPT_PATH)
 
 def exec_time(func):
     def wrapper(*args, **kwargs):
@@ -9,7 +11,7 @@ def exec_time(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
-        print(f"{func.__name__} executed in {elapsed_time:.4f} seconds")
+        print(f"Function {func.__name__} executed in {elapsed_time:.4f} seconds")
         return result
     return wrapper
 
@@ -20,7 +22,7 @@ class Fetcher:
     
     @exec_time
     def extract_text(self, ocr_results):
-        with open("prompt.txt", 'r') as f:
+        with open(LLM_PROMPT_PATH, 'r') as f:
             prompt = f.read()
         prompt = PromptTemplate.from_template(prompt)
         final_prompt = prompt.format(input=ocr_results[:25])
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ HEADERS = {
 LLM_MODEL = os.getenv("LLM_MODEL", "llama3.1")
 LLM_BASE_URL = os.getenv("LLM_BASE_URL", "http://localhost:11434")
 LLM_TEMPERATURE = float(os.getenv("LLM_TEMPERATURE", "0.1"))
+LLM_PROMPT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.getenv("LLM_PROMPT_PATH", "prompts"))
